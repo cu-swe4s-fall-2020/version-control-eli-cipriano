14208609e04e053a54bd376b364c984abee26de8
eli-cipriano <elia.cipriano@colorado.edu>
2020-09-07 23:18:56 -0600

    added a bunch of functions, still trying to get outputs

--- math_lib.py
+++ math_lib.py
@@ -1,2 +1,22 @@
+import math
 def div(a, b):
-    return a/b
+    if b == 0:
+        return -Inf
+    else:
+        return a/b
+
+def sum(a, b):
+    return print(a+b)
+
+def prod(a, b):
+    return a*b
+
+def exp(a,b):
+    return a**b
+
+def sin(a, b):
+    #b is either R or D, for radian or deg.
+    if b.lower() == 'd':
+        a = a/180*3.1415926
+
+    
