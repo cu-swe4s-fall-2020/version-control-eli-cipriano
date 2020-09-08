f8e483d466681457db34e57cf3ef35ec25a562d4
eli-cipriano <elia.cipriano@colorado.edu>
2020-09-07 23:59:38 -0600

    unmerging with master

--- math_lib.py
+++ math_lib.py
@@ -1,25 +1,2 @@
 def div(a, b):
-    if b == 0:
-        return -Inf
-    else:
-        return a/b
-
-
-def sum(a, b):
-    return a+b
-
-
-def prod(a, b):
-    return a*b
-
-
-def exp(a, b):
-    return a**b
-
-
-def sin(a, b):
-    # b is either R or D, for radian or deg.
-    if b.lower() == 'd':
-        sd = a/180*3.1415926
-    sd = sd - sd**3/(6) + sd**5/(120) - sd**7/(5040) + sd**9/(362880)
-    return sd
+    return a/b
