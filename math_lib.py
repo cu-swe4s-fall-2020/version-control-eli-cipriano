3b99603344b5d073c815a533a18f92121f916f00
eli-cipriano <elia.cipriano@colorado.edu>
2020-09-07 23:56:29 -0600

    completed math_lib, but returns do not display.

--- math_lib.py
+++ math_lib.py
@@ -1,22 +1,25 @@
-import math
 def div(a, b):
     if b == 0:
         return -Inf
     else:
         return a/b
 
+
 def sum(a, b):
-    return print(a+b)
+    return a+b
+
 
 def prod(a, b):
     return a*b
 
-def exp(a,b):
+
+def exp(a, b):
     return a**b
 
-def sin(a, b):
-    #b is either R or D, for radian or deg.
-    if b.lower() == 'd':
-        a = a/180*3.1415926
 
-    
+def sin(a, b):
+    # b is either R or D, for radian or deg.
+    if b.lower() == 'd':
+        sd = a/180*3.1415926
+    sd = sd - sd**3/(6) + sd**5/(120) - sd**7/(5040) + sd**9/(362880)
+    return sd
